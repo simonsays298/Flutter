fcb07452b38e2362bbac081d6d0b40021e3e67ed
simonsays298 <45179455+simonsays298@users.noreply.github.com>
2020-12-01 01:05:28 +0200

    updated guess_my_number.dart

--- lib/src/guess_my_number.dart
+++ lib/src/guess_my_number.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -10,33 +10,37 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Homepage(title: 'Guess My Number'),
     );
   }
 }
 
 class Homepage extends StatefulWidget {
-  Homepage({Key key, this.title}) : super(key: key);
-  @override
+  const Homepage({Key key, this.title}) : super(key: key);
   final String title;
 
+  @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-  @override
-  String _msg = 'I\'m thinking of a number betweeen 1 and 100';
-  String _msg2 = 'It\'s your turn to guess my number';
-  String _res = '';
-  String guess ='Guess';
-  int number = new Random().nextInt(100);
+  final String _msg = 'I\'m thinking of a number between 1 and 100';
+  final String _msg2 = 'It\'s your turn to guess my number';
+  String _msg3 = '';
+  String _value = '';
+  int _givenNumber;
+
+  String guess = 'Guess';
+  int number = Random().nextInt(100);
+
   String error = '';
-  bool show = true;
+  bool show = false;
+  final TextEditingController myController = TextEditingController();
 
+  String inputString = '';
 
-  String inputString = "";
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -55,68 +59,119 @@ class _HomepageState extends State<Homepage> {
               child: Text(_msg2),
             ),
             Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Visibility(
+                child: Text(
+                  'You tried ' + _value + '\n' + _msg3,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                visible: show,
+              ),
+            ),
+            Padding(
               padding: const EdgeInsets.all(40.0),
               child: Card(
                 child: Column(
                   children: <Widget>[
-                    Text("Try a number !!!"),
+                    const Text('Try a number !!!'),
                     TextField(
                       keyboardType: TextInputType.number,
+                      // decoration: InputDecoration(
+                      //   errorText: error,
+                      // ),
+                      // ignore: always_specify_types
                       inputFormatters: [
                         FilteringTextInputFormatter.allow(
-                            RegExp(r"\d+([\.]\d+)?")),
+                            RegExp(r'\d+([\.]\d+)?')),
                       ],
+                      controller: myController,
                       onChanged: (String value) {
-                        _res = value;
-
-                        print(number);
+                        _value = value;
+                        // print(number);
                       },
                     ),
                     RaisedButton(
                         child: Text(guess),
                         textColor: Colors.blue,
                         onPressed: () {
-                          if (int.parse(_res) == number) {
-                            showDialog<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("You guessed right!"),
-                                  content: SingleChildScrollView(
-                                    child: Text(_res),
-                                  ),
-                                  actions: <Widget>[
-                                    Row(
-                                      children: [
-                                        TextButton(
-                                          onPressed: () {
+                          setState(() {
+                            if (guess == 'Reset') {
+                              show = false;
+                              guess = 'Guess';
+                              _value = '';
+                              myController.clear();
+                              number = Random().nextInt(100);
 
-                                            Navigator.of(context).pop();
-
-                                          },
-                                          child: Text("Try again!"),
-                                        ),
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-
-                                          },
-
-                                          child: Text("OK"),
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                );
-                              },
-                            );
-                          } else {
-                            if (int.parse(_res) < number) {
-                              print("Higher");
                             } else {
-                              print("Lower");
+                              _givenNumber = int.tryParse(_value) ?? 0;
+                              if (_givenNumber == 0) {
+                                // error = 'insert a proper number';
+                                show = false;
+                              } else {
+                                // error = '';
+                                // print(_givenNumber);
+                                if (_givenNumber == number) {
+                                  // print('A');
+                                  show = true;
+                                  _msg3 = 'You guessed right';
+                                  showDialog<void>(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('You guessed right!'),
+                                        content: SingleChildScrollView(
+                                          child: Text('$_givenNumber'),
+                                        ),
+                                        actions: <Widget>[
+                                          Row(
+                                            // ignore: always_specify_types
+                                            children: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  setState(() {
+                                                    guess = 'Guess';
+                                                    show = false;
+                                                    myController.clear();
+                                                    _value = '';
+                                                    number =
+                                                        Random().nextInt(100);
+                                                    // print(number);
+                                                  });
+
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text('Try again!'),
+                                              ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  setState(() {
+                                                    guess = 'Reset';
+                                                  });
+
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                } else {
+                                  if (_givenNumber < number) {
+                                    // print('B');
+                                    _msg3 = 'Try higher!';
+                                    show = true;
+                                  } else {
+                                    _msg3 = 'Try lower';
+                                    show = true;
+                                  }
+                                }
+                              }
                             }
-                          }
+                          });
                         }),
                   ],
                 ),
@@ -129,5 +184,12 @@ class _HomepageState extends State<Homepage> {
         ),
       ),
     );
+  }
+
+  void tryAgain() {}
+
+  void pressOk() {
+    // guess = 'Reset';
+    // show = false;
   }
 }
