9b1fa2f74813833ce18dac2bc7d11c5fbb3639f9
simonsays298 <45179455+simonsays298@users.noreply.github.com>
2020-12-01 12:45:23 +0200

    updated number_shape.dart

--- lib/src/number_shape.dart
+++ lib/src/number_shape.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:powers/powers.dart';
@@ -10,34 +9,35 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Homepage(title: 'Number Shapes'),
     );
   }
 }
 
 class Homepage extends StatefulWidget {
-  Homepage({Key key, this.title}) : super(key: key);
-  @override
+  const Homepage({Key key, this.title}) : super(key: key);
   final String title;
 
+  @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-  @override
-  String _msg = 'Please input a number to see if it is square or triangle';
+  final String _msg =
+      'Please input a number to see if it is SQUARE or TRIANGULAR';
   String _value = '';
-  String _cube = 'is TRIANGLE';
-  String _square = 'is SQUARE';
-  String _both = 'is both SQUARE and TRINAGLUAR';
-  String _neither = 'is neither SQUARE or TRINAGLUAR';
+  final String _cube = 'is TRIANGLE';
+  final String _square = 'is SQUARE';
+  final String _both = 'is both SQUARE and TRIANGULAR';
+  final String _neither = 'is neither SQUARE nor TRIANGULAR';
   String _res = '';
   String error = '';
   bool show = true;
   double number;
-  String inputString = "";
+  String inputString = '';
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -45,20 +45,33 @@ class _HomepageState extends State<Homepage> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(14.0),
         child: Column(
           children: <Widget>[
-            Text(
-              _msg,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network(
+                'https://image.freepik.com/free-vector/flat-thinking-concept_23-2148163823.jpg',
+                height: 200.0,
+                width: 200.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                _msg,
+                style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
             ),
             TextField(
               onChanged: (String value) {
                 _value = value;
-                number = double.tryParse(_value) ?? double.maxFinite;
               },
               keyboardType: TextInputType.number,
+              // ignore: always_specify_types
               inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp(r"\d+([\.]\d+)?")),
+                FilteringTextInputFormatter.allow(RegExp(r'\d+([\.]\d+)?')),
               ],
               decoration: InputDecoration(
                 errorText: error,
@@ -68,18 +81,20 @@ class _HomepageState extends State<Homepage> {
               child: Container(),
             ),
             Column(
+              // ignore: always_specify_types
               children: [
                 RaisedButton(
                   color: Colors.lightGreen,
-                  shape: CircleBorder(),
+                  shape: const CircleBorder(),
                   splashColor: Colors.greenAccent,
-                  animationDuration: Duration(seconds: 8),
+                  animationDuration: const Duration(seconds: 12),
                   colorBrightness: Brightness.light,
                   child: Align(
                     alignment: Alignment.bottomCenter,
                     child: Column(
+                      // ignore: prefer_const_literals_to_create_immutables
                       children: <Widget>[
-                        Icon(
+                        const Icon(
                           Icons.check,
                           color: Colors.white,
                         ),
@@ -88,24 +103,13 @@ class _HomepageState extends State<Homepage> {
                   ),
                   onPressed: () {
                     setState(() {
-                      if (number != double.maxFinite) {
+                      number = double.tryParse(_value) ?? 0;
+                      if (number != 0) {
                         show = true;
                         error = '';
-                        if (number.isSquare && number.isCube) {
-                          _res = 'Number ' + _value + ' ' + _both;
-                        } else {
-                          if (number.isSquare) {
-                            _res = 'Number ' + _value + ' ' + _square;
-                          } else {
-                            if (number.isCube) {
-                              _res = 'Number ' + _value + ' ' + _cube;
-                            } else {
-                              _res = 'Number ' + _value + ' ' + _neither;
-                            }
-                          }
-                        }
+                        checkSqrOrCube(number);
                       } else {
-                        error = "Enter a valid number";
+                        error = 'Enter a valid number';
                         show = false;
                       }
                       if (show == true) {
@@ -113,9 +117,14 @@ class _HomepageState extends State<Homepage> {
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
-                              title: Text(_value),
+                              backgroundColor: Color.fromRGBO(204, 255, 255, 0.8),
+                              title: Text(
+                                _value,
+                                textAlign: TextAlign.center,
+                              ),
                               content: SingleChildScrollView(
-                                child: Text(_res),
+                                child: Text(_res,
+                                  textAlign: TextAlign.center,),
                               ),
                             );
                           },
@@ -130,5 +139,21 @@ class _HomepageState extends State<Homepage> {
         ),
       ),
     );
+  }
+
+  void checkSqrOrCube(double number) {
+    if (number.isSquare && number.isCube) {
+      _res = 'Number ' + _value + ' ' + _both;
+    } else {
+      if (number.isSquare) {
+        _res = 'Number ' + _value + ' ' + _square;
+      } else {
+        if (number.isCube) {
+          _res = 'Number ' + _value + ' ' + _cube;
+        } else {
+          _res = 'Number ' + _value + ' ' + _neither;
+        }
+      }
+    }
   }
 }
